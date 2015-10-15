1556702c019549f388b1d183847a51862592e54c
Szymon <Szymon@150.254.145.72>
2015-10-15 20:27:17 +0200

    calendar fix

--- components/com_repertoire/views/events/tmpl/default.php
+++ components/com_repertoire/views/events/tmpl/default.php
@@ -46,16 +46,13 @@ if ($this->params->get('show_page_heading', 1)) : ?>
 </form>
 
 <script type="text/javascript">
-    jQuery(document).ready(function () {
-        jQuery('.hasTooltip').tooltip({"html": true, "container": "body"});
-    });
     Calendar._DN = ["Niedziela", "Poniedzia\u0142ek", "Wtorek", "\u015aroda", "Czwartek", "Pi\u0105tek", "Sobota", "Niedziela"];
     Calendar._SDN = ["N", "Pn", "Wt", "\u015ar", "Cz", "Pt", "So", "N"];
     Calendar._FD = 0;
     Calendar._MN = ["stycze\u0144", "luty", "marzec", "kwiecie\u0144", "maj", "czerwiec", "lipiec", "sierpie\u0144", "wrzesie\u0144", "pa\u017adziernik", "listopad", "grudzie\u0144"];
     Calendar._SMN = ["Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Pa\u017a", "Lis", "Gru"];
     Calendar._TT = {"INFO": "O kalendarzu", "ABOUT": "DHTML Date\/Time Selector\n(c) dynarch.com 2002-2005 \/ Author: Mihai Bazon\nFor latest version visit: http:\/\/www.dynarch.com\/projects\/calendar\/\nDistributed under GNU LGPL.  See http:\/\/gnu.org\/licenses\/lgpl.html for details.\n\nWyb\u00f3r daty:\n- U\u017cyj strza\u0142ek &#171; i &#187;, aby wybra\u0107 rok\n- U\u017cyj przycisk\u00f3w &lt; i &gt; aby wskaza\u0107 miesi\u0105c\n- Przytrzymaj przycisk myszki na dowolnym z powy\u017cszych przycisk\u00f3w, aby zaznaczy\u0107 szybciej.", "ABOUT_TIME": "\n\nTime selection:\n- Click on any of the time parts to increase it\n- or Shift-click to decrease it\n- or click and drag for faster selection.", "PREV_YEAR": "Przyci\u015bnij, by przej\u015b\u0107 do poprzedniego roku. Przyci\u015bnij i przytrzymaj, by przej\u015b\u0107 do listy lat.", "PREV_MONTH": "Przyci\u015bnij, by przej\u015b\u0107 do poprzedniego miesi\u0105ca. Przyci\u015bnij i przytrzymaj, by przej\u015b\u0107 do listy miesi\u0119cy.", "GO_TODAY": "Przejd\u017a do dzi\u015b", "NEXT_MONTH": "Przyci\u015bnij, by przej\u015b\u0107 do nast\u0119pnego miesi\u0105ca. Przyci\u015bnij i przytrzymaj, by przej\u015b\u0107 do listy miesi\u0119cy.", "SEL_DATE": "Wska\u017c dat\u0119.", "DRAG_TO_MOVE": "Przyci\u015bnij i przeci\u0105gnij, by przesun\u0105\u0107", "PART_TODAY": " Dzi\u015b ", "DAY_FIRST": "Najpierw %s", "WEEKEND": "0,6", "CLOSE": "Zamknij", "TODAY": "Dzi\u015b", "TIME_PART": "[Shift] + przyci\u015bnij lub przeci\u0105gnij, by zmieni\u0107 warto\u015b\u0107.", "DEF_DATE_FORMAT": "%Y-%m-%d", "TT_DATE_FORMAT": "%a, %b %e", "WK": "tydz.", "TIME": "Czas:"};
-    jQuery(document).ready(function ($) {
+    jQuery(document).ready(function () {
         Calendar.setup({
             // Id of the input field
             inputField: "jform_date",
